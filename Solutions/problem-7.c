29b405ed5afbdaf8d1560ff3f046c80c5877570a
Tanvir  Ahmed <ruman777ahmed@gmail.com>
2024-02-23 00:50:17 +0600

    Update problem-7.c

---
+++ Solutions/problem-7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+int n;
+printf("Enter an integer: ");
+scanf("%d", &n);
+
+for (int i = 1; i <= n; i++) {
+for (int j = 1; j <= n - i; j++) {
+printf(" ");
+}
+for (int k = 1; k <= i * 2 - 1; k++) {
+printf("*");
+}
+printf("\n");
+}
+
+return 0;
+}
